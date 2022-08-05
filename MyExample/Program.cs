30ceb4009776b3091f2fa38bfd1c950b3adaec9c
Ян Гальперин <yan@MacBook-Air-An.local>
2022-08-05 15:04:17 +0300

    My example

---
+++ MyExample/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("введите имя пользователя: ");
+string usermame = Console.ReadLine();
+Double numberA = new Random().Next(1, 100);
+Console.WriteLine(numberA);
+Double numberB = new Random().Next(1, 100);
+Console.WriteLine(numberB);
+Double result = numberA / numberB;
+if(result < 1)
+{
+Console.WriteLine(result);
+Console.Write(usermame);
+Console.WriteLine(", херовый результат!");
+}
+else
+{
+    Console.WriteLine(result);
+    Console.Write(usermame);
+    Console.WriteLine(" - молодец");
+}
